e987ee00105803fc64776cd841604e41d6e3d066
Christoph Wedenig <christoph@wedenig.org>
2017-07-14 11:17:24 +0200

    added support for e2e testing

---
+++ test/spec.js
@@ -0,0 +1,11 @@
+const config = require('mikro-config');
+// spec.js
+describe('DiffVisualizer', function() {
+  it('should have a title', function() {
+    browser.waitForAngularEnabled(false);
+    var url = 'http://localhost:9999';
+    browser.get(url);
+
+    expect(browser.getTitle()).toEqual('Diff Visualizer');
+  });
+});
