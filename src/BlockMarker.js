4edd409d910107413dca4fa861835bcc6f4b0b6b
Christoph Wedenig <christoph@wedenig.org>
2017-08-07 16:41:59 +0200

    add BlockMarker for future use

---
+++ src/BlockMarker.js
@@ -0,0 +1,32 @@
+import Marker from './Marker';
+
+class BlockMarker extends Marker {
+
+  constructor(id, blockName, position) {
+    super();
+    this.id = id;
+    this.blockName = blockName;
+    this.position = position;
+    this.isEndMarker = false;
+  }
+
+  // setIsEndMarker(isEndMarker) {
+  //   this.isEndMarker = isEndMarker;
+  // }
+
+  generateTag() {
+    if (this.isEndMarker) {
+      return '</span>';
+    } else {
+      return '<span class="blockmarker" data-blockid="${this.id}">';
+    }
+  }
+
+  createEndMarker(length) {
+    var endmarker = new BlockMarker(this.id, this.blockName, this.position + length);
+    endmarker.setIsEndMarker(true);
+
+    return endmarker;
+  }
+
+} export default BlockMarker;
