9a584fe19f99598a7ca85b55e29b3a196c04eb76
Christoph Wedenig <christoph@wedenig.org>
2017-08-16 16:20:42 +0200

    added bootbox to webpack copy config

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,31 @@ var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/DiffVisualizer.js',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    filename: 'diffvisualizer.js'
-  },
-  module: {
-    loaders: [{
-      test: path.join(__dirname, 'es6'),
-      loader: 'babel-loader'
-    }]
-  },
-  plugins: [
+    entry: './src/DiffVisualizer.js',
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        filename: 'diffvisualizer.js'
+    },
+    module: {
+        loaders: [{
+            test: path.join(__dirname, 'es6'),
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
         new CopyWebpackPlugin([
 
             // {output}/to/file.txt
             {
-              context: __dirname,
-              from: 'node_modules/mark.js/dist/jquery.mark.min.js',
-              to: 'jquery.mark.min.js'
+                context: __dirname,
+                from: 'node_modules/mark.js/dist/jquery.mark.min.js',
+                to: 'jquery.mark.min.js'
             },
+            {
+                context: __dirname,
+                from: 'node_modules/bootbox/bootbox.min.js',
+                to: 'bootbox.min.js'
+            }
 
         ], {
             // By default, we only copy modified files during
