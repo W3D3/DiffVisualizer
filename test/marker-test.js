93f8736439c936d006c5b51a9931e9648e4efb1c
Christoph Wedenig <christoph@wedenig.org>
2017-09-20 14:06:22 +0200

    fixed test for new marker position

--- test/marker-test.js
+++ test/marker-test.js
@@ -14,41 +14,41 @@ import Marker from '../src/Marker';
 
 describe('Marker', () => {
 
-  describe('#generating a tag', () => {
+    describe('#generating a tag', () => {
 
-    it('returns empty closing tag if endmarker', () => {
-      var marker = new Marker(1, 44, 'INSERT', true, 'src');
-      marker.generateTag().should.equal('</span>');
+        it('returns empty closing tag if endmarker', () => {
+            var marker = new Marker(1, 44, 'INSERT', true, 'src');
+            marker.generateTag().should.equal('</span>');
+        });
+
+        it('return valid marker html if no endmarker', () => {
+            var marker = new Marker(1, 44, 'INSERT', false, 'src');
+            var htmlstring = marker.generateTag();
+            expect(htmlstring).html.to.equal('<span class="INSERT scriptmarker" id="src1">');
+        });
+
+
     });
 
-    it('return valid marker html if no endmarker', () => {
-      var marker = new Marker(1, 44, 'INSERT', false, 'src');
-      var htmlstring = marker.generateTag();
-      expect(htmlstring).html.to.equal('<span class="INSERT scriptmarker" id="src1">');
+    describe('#properties', () => {
+        it('marker can be bound', () => {
+            var marker = new Marker(1, 44, 'INSERT', false, 'src');
+            marker.bindToId(2);
+            var htmlstring = marker.generateTag();
+            expect(htmlstring).to.contain('data-boundto="dst2"');
+        });
+
+        it('generated endmarker is the same in all other attributes', () => {
+            var marker = new Marker(1, 44, 'INSERT', false, 'src');
+            marker.bindToId(2);
+            var endMarker = marker.createEndMarker(12); //12 is length
+            expect(endMarker.id).to.equal(marker.id);
+            expect(endMarker.position).to.equal(12);
+            expect(endMarker.type).to.equal(marker.type);
+            expect(endMarker.bind).to.equal(marker.bind);
+            expect(endMarker.sourceType).to.equal(marker.sourceType);
+            expect(endMarker.metaDataMarkup).to.equal(marker.metaDataMarkup);
+        });
+
     });
-
-
-  });
-
-  describe('#properties', () => {
-    it('marker can be bound', () => {
-      var marker = new Marker(1, 44, 'INSERT', false, 'src');
-      marker.bindToId(2);
-      var htmlstring = marker.generateTag();
-      expect(htmlstring).to.contain('data-boundto="dst2"');
-    });
-
-    it('generated endmarker is the same in all other attributes', () => {
-      var marker = new Marker(1, 44, 'INSERT', false, 'src');
-      marker.bindToId(2);
-      var endMarker = marker.createEndMarker(12); //12 is length
-      expect(endMarker.id).to.equal(marker.id);
-      expect(endMarker.position).to.equal(marker.position+12);
-      expect(endMarker.type).to.equal(marker.type);
-      expect(endMarker.bind).to.equal(marker.bind);
-      expect(endMarker.sourceType).to.equal(marker.sourceType);
-      expect(endMarker.metaDataMarkup).to.equal(marker.metaDataMarkup);
-    });
-
-  });
 });
